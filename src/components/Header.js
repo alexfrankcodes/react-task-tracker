c5cbef560cd6c96bce70a1432b0ad74708604682
Alex Frank <alexfrankcodes@gmail.com>
2021-01-25 14:32:12 -0800

    Implemented ability to toggle task form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title }) => (
+const Header = ({ title, onAdd, showAdd }) => (
   <header className="header">
     <h1>{title}</h1>
-    <Button text="Add" />
+    <Button text={showAdd ? "Close" : "Add"} onClick={onAdd} />
   </header>
 );
 
@@ -14,6 +14,8 @@ Header.defaultProps = {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  onAdd: PropTypes.func,
+  showAdd: PropTypes.func,
 };
 
 export default Header;
